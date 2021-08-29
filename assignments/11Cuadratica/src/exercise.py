b6400bd89da26f149acdbb3a51b23632222063df
Rolando Martínez <A01177704@itesm.mx>
2021-08-29 16:07:42 -0500

    Entrega de la tarea

--- assignments/11Cuadratica/src/exercise.py
+++ assignments/11Cuadratica/src/exercise.py
@@ -2,25 +2,25 @@ import math
 
 def main():
     # Escribe tu código abajo de esta línea
-    valorA=int(input("Da el valor de a: "))
-    valorB=int(input("Da el valor de b: "))
-    valorC=int(input("Da el valor de c: "))
-    if (valorA==0) and (valorB==0):
+    a=int(input("Da el valor de a: "))
+    b=int(input("Da el valor de b: "))
+    c=int(input("Da el valor de c: "))
+    if (a==0) and (b==0):
         print("No tiene solucion")
-    elif (valorA==0):
-        raiz=-valorC/valorB
+    elif (a==0):
+        raiz=-c/b
         print(raiz)
     else:
-        discrim=valorB**2-4*valorA*valorC
+        discrim=b**2-4*a*c
         if (discrim)>0:
-            x1=(-valorB+math.sqrt(discrim))/(2*valorA)
-            x2=(-valorB-math.sqrt(discrim))/(2*valorA)
+            x1=(-b+math.sqrt(discrim))/(2*a)
+            x2=(-b-math.sqrt(discrim))/(2*a)
             print(x1)
             print(x2)
         elif (discrim < 0):
             print("Raices complejas")
         else:
-            x=-valorB/(2*valorA)
+            x=-b/(2*a)
             print(x)
 
 if __name__ == '__main__':
